11e12a4ff347324a4559e4e09f0d6b54824c9c4d
Sea <hjw_redeption@outlook.com>
2016-06-04 09:59:38 -1200

    restore optimizer....

--- lift/Optimizer.py
+++ lift/Optimizer.py
@@ -1,2 +1,19 @@
+"""
+Optimizer module for Lift JS.
+"""
+
+
+def fold_constant(ast):
+    # TODO
+    return ast
+
+
+def eliminate_unreachable(ast):
+    # TODO
+    return ast
+
+
 def optimize(ast):
-    pass
+    ret = fold_constant(ast)
+    ret = eliminate_unreachable(ret)
+    return ast
